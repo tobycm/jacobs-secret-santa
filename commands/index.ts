2490ad09b8b43379c707989c6ccdca2ad457218c
Toby Cm <toby@vsus.app>
2026-01-23 21:07:57 -0800

    wrong name

--- commands/index.ts
+++ commands/index.ts
@@ -1,14 +1,14 @@
 import ask from "./ask";
 import type { Command } from "./command";
 import exfiltrate from "./exfiltrate";
-import minecraft from "./minecraft";
 import shuffle from "./shuffle";
 import vaporize from "./vaporize";
+import whitelist from "./whitelist";
 
 export default {
   ask,
   vaporize,
   shuffle,
   exfiltrate,
-  minecraft,
+  whitelist,
 } as Record<string, Command>;
